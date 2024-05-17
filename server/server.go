1e035187ca5b0f4bf462126d6629639247c47cc2
HEMANT KUMAR <hkbiet@gmail.com>
2024-05-17 17:25:29 +0530

    feat: change ports (#4)
    Enabled service on previous ports along with new ports.
    
    Closes #3
    
    Co-authored-by: Markus Tacker <markus.tacker@nordicsemi.no>

--- server/server.go
+++ server/server.go
@@ -1,15 +1,12 @@
 package main
 
 import (
+	"coap-server/launch"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 
 	"coap-server/internal"
-
-	piondtls "github.com/pion/dtls/v2"
-	coap "github.com/plgd-dev/go-coap/v3"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 )
@@ -30,6 +27,20 @@ func main() {
 		"The network to use, `udp4` or `udp6`.")
 	flag.Parse()
 
+	var flagValues = launch.ServerFlags{
+		Address:  *address,
+		Password: *password,
+		Dtls:     *dtls,
+		Network:  *network,
+	}
+
+	var ports = launch.ServerPorts{
+		OldUdpPort:  5688,
+		NewUdpPort:  5683,
+		OldDtlsPort: 5689,
+		NewDtlsPort: 5684,
+	}
+
 	storageConnectionString, ok := os.LookupEnv("STORAGE_CONNECTION_STRING")
 	if !ok {
 		log.Fatal("the environment variable 'STORAGE_CONNECTION_STRING' could not be found")
@@ -46,24 +57,10 @@ func main() {
 
 	r := internal.NewServer(storageClient, containerName)
 
-	udpPort := 5688
-	dTLSPort := 5689
-	udpAddr := fmt.Sprintf("%s:%d", *address, udpPort)
-	dtlsAddr := fmt.Sprintf("%s:%d", *address, dTLSPort)
+	// old ports
+	go launch.Server(flagValues, ports.OldUdpPort, ports.OldDtlsPort, r)
 
-	if *dtls {
-		log.Printf("dTLS %s Server listening on: %s\n", *network, dtlsAddr)
-		log.Printf("dTLS PSK: %s\n", *password)
-		log.Fatal(coap.ListenAndServeDTLS(*network, dtlsAddr, &piondtls.Config{
-			PSK: func(hint []byte) ([]byte, error) {
-				log.Printf("Client's hint: %s \n", hint)
-				return []byte(*password), nil
-			},
-			PSKIdentityHint: []byte("Pion DTLS Client"),
-			CipherSuites:    []piondtls.CipherSuiteID{piondtls.TLS_PSK_WITH_AES_128_CCM_8},
-		}, r))
-	} else {
-		log.Printf("%s Server listening on: %s\n", *network, udpAddr)
-		log.Fatal(coap.ListenAndServe(*network, udpAddr, r))
-	}
+	// new ports
+	launch.Server(flagValues, ports.NewUdpPort, ports.NewDtlsPort, r)
+
 }
